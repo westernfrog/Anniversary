0b0e9d8249234c254669d12e81fc91adf129e1c8
Aman Singh <haaamansingh007@gmail.com>
2022-08-20 12:55:54 +0530

    Updated

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-var popoverTriggerList = [].slice.call(
-  document.querySelectorAll('[data-bs-toggle="popover"]')
+var tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
 );
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-  return new bootstrap.Popover(popoverTriggerEl);
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl);
 });
